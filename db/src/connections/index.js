16b9ce0283e14d88939859a14b4335a00b9b9ff3
Mauricio Fow <mauriciofow@gmail.com>
2019-12-05 16:30:55 -0500

    jawsdb2

--- db/src/connections/index.js
+++ db/src/connections/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('yakdd3tuwv5qtzcp', 'b1sl6wn4sic4fgg3', 'vdsrbirnmws4v3v2', {
-	host    : 'ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+const sequelize = new Sequelize('cart', 'root', 'internetes', {
+	host    : 'localhost',
 	dialect : 'mysql'
 });
 
