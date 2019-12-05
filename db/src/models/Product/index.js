a0f9ff93252e6d0f095dea5398b4979ae90890e1
Mauricio Fow <mauriciofow@gmail.com>
2019-12-05 17:19:05 -0500

    jawsdb working

--- db/src/models/Product/index.js
+++ db/src/models/Product/index.js
@@ -1,25 +1,31 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../../connections');
 
-module.exports = sequelize.define('Product', {
-	id       : {
-		type          : Sequelize.INTEGER(11),
-		allowNull     : false,
-		autoIncrement : true,
-		primaryKey    : true
+module.exports = sequelize.define(
+	'Product',
+	{
+		id       : {
+			type          : Sequelize.INTEGER(11),
+			allowNull     : false,
+			autoIncrement : true,
+			primaryKey    : true
+		},
+		title    : {
+			type      : Sequelize.STRING(35),
+			allowNull : false,
+			unique    : true
+		},
+		price    : {
+			type      : Sequelize.DECIMAL(10, 2),
+			allowNull : false
+		},
+		img      : {
+			type      : Sequelize.STRING(200),
+			allowNull : false
+		},
+		quantity : Sequelize.INTEGER(11)
 	},
-	title    : {
-		type      : Sequelize.STRING(35),
-		allowNull : false,
-		unique    : true
-	},
-	price    : {
-		type      : Sequelize.DECIMAL(10, 2),
-		allowNull : false
-	},
-	img      : {
-		type      : Sequelize.STRING(200),
-		allowNull : false
-	},
-	quantity : Sequelize.INTEGER(11)
-});
+	{
+		tableName : 'products'
+	}
+);
